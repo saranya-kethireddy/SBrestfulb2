b62a3d7b3c9b0e5ae1ea1ce1dc67b96e9096e417
skethireddy2 <skethireddy2@IN-PG02LCPA>
2021-11-02 14:37:16 +0530

    find a student

--- SBrestb2/src/main/java/dxc/b2/rest/StudentService.java
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentService.java
@@ -26,4 +26,15 @@ public class StudentService {
 		createStudents();
 		return students;
 	}
+	
+	public Student findStudent(int id) {
+
+		Student student=  students.stream()   //convert the students list to a stream
+				.filter(s -> s.getId() == id )
+				.findFirst()
+				.get();
+
+		return student;
+	}
+
 }
